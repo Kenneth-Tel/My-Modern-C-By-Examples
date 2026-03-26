08053f4a8f51162837abadc19df3fefd5dd7ab4b
Kevin- <kenneth.telling@gmail.com>
2026-03-26 17:48:27 +0100

    Added the value vs representation

--- values_and_representation_test.c
+++ values_and_representation_test.c
@@ -1,38 +1,8 @@
-# My-Modern-C-By-Examples
-My own little artifact repository from reading Modern C, third Edition.
-
-## Code Snippets
-### String Arrays and Concatenation
-**Explanation**: The **C Standard** specifies that consecutive string literals are concatenated into a larger string.
-> [!NOTE]
-> While it's the compilers job to concatenate it, the C Standard demands this behaviour: Defined Behaviour.
-```C
-#include <stdio.h>
-#include <stdlib.h> // for EXIT_SUCCESS
-
-int main(int argc, [[maybe_unused]] char* argv[argc + 1]) {
-    char message[] = "Hello, " "World! "
-        "Welcome to my program! " "Its a cool program right?";
-
-    printf("%s\n", message);
-
-    message[0] = 'W'; // The code defines message as an array of characters, not a pointer to a string literal.
-    printf("%s\n", message);
-
-    return EXIT_SUCCESS;
-}
-```
-Produces:
-```bash
-Hello, World! Welcome to my program! Its a cool program right?
-Wello, World! Welcome to my program! Its a cool program right?
-```
-Takeaway 5.3 #1 Consecutive string literals are concatenated.
-
-### Values and Representation.
-Note: It started off as a C abstract machine note, but then it diverges into something else.
-
-```C
+/* Name: values_and_representation_test.c
+ * Author: Kevin
+ * - Link: https://github.com/Kenneth-Tel
+ * Tags: [Representation, IEEE 754]
+ */
 #include <stdio.h>
 #include <stdlib.h> // for EXIT_SUCCESS
 
@@ -107,4 +77,4 @@ int main(int argc, [[maybe_unused]] char* argv[argc + 1]) {
 
     return EXIT_SUCCESS;
 }
-```
+
