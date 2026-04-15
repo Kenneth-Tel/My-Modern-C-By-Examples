9784e6b95a94fd1654fd9f9524601bf430364192
Kevin- <kenneth.telling@gmail.com>
2026-04-15 16:30:03 +0200

    Fixed some terminology

--- string_pointer_vs_array.c
+++ string_pointer_vs_array.c
@@ -1,7 +1,7 @@
 /* Name: string_pointer_vs_array.c
  * Author: Kevin
  * - Link: https://github.com/Kenneth-Tel
- * Tags: [unnamed string literals, array, pointer]
+ * Tags: [unnamed string literals, array, pointer, const, const-qualified]
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,9 +16,9 @@ int main(int argc, [[maybe_unused]] char* argv[argc + 1]) {
 
     ptrName[0] = 'L'; // Crashes. (ASAN error if -fsanitize=address)
 
-    // To simulate the char* with VLA, we can create a const-qualified object, to the constant "Kevin Telling"
+    // To simulate the char* with an array, we can create a const-qualified object, to the constant "Kevin Telling"
     char const arrConstName[] = "Kevin " "Telling";
-    arrConstName[0] = 'L'; // Will not crash, because the program wont compile. So try to use this.
+    arrConstName[0] = 'L'; // Will not crash, because the program wont compile. Try best to use this, so no runtime crashes happen.
 
     return EXIT_SUCCESS;
 }
