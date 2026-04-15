f5439e31a4a549382829572a877d899762c5b3e7
Kevin- <kenneth.telling@gmail.com>
2026-04-15 16:20:46 +0200

    Added a const-qualified VLA the the example for C23 features.

--- string_pointer_vs_array.c
+++ string_pointer_vs_array.c
@@ -16,6 +16,10 @@ int main(int argc, [[maybe_unused]] char* argv[argc + 1]) {
 
     ptrName[0] = 'L'; // Crashes. (ASAN error if -fsanitize=address)
 
+    // To simulate the char* with VLA, we can create a const-qualified object, to the constant "Kevin Telling"
+    char const arrConstName[] = "Kevin " "Telling";
+    arrConstName[0] = 'L'; // Will not crash, because the program wont compile. So try to use this.
+
     return EXIT_SUCCESS;
 }
 
